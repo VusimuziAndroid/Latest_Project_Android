f722686df65da61dc47ec38a7fb7130198c04a15
VusimuziAndroid <vsngwenya15@gmail.com>
2016-06-30 07:51:44 +0200

    Second Commit

--- app/src/main/java/android/latest_project_android/Home.java
+++ app/src/main/java/android/latest_project_android/Home.java
@@ -8,9 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 public class Home extends AppCompatActivity {
-
+    TabHost tabhost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +27,30 @@ public class Home extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
 
+        tabhost = (TabHost) findViewById(R.id.tabHost);
+        tabhost.setup();
+        TabHost.TabSpec  firstText = tabhost.newTabSpec("HOME");
+        firstText.setContent(R.id.HOME);
+        firstText.setIndicator("",getDrawable(R.drawable.ic_home_white_48dp));
+        tabhost.addTab(firstText);
+        TabHost.TabSpec secondText = tabhost.newTabSpec("GROUPS");
+        secondText.setContent(R.id.GROUPS);
+        secondText.setIndicator("",getDrawable(R.drawable.ic_person_white_48dp));
+        tabhost.addTab(secondText);
+        TabHost.TabSpec thirdText = tabhost.newTabSpec("CHATS");
+        thirdText.setContent(R.id.CHATS);
+        thirdText.setIndicator("",getDrawable(R.drawable.ic_drafts_white_48dp));
+        tabhost.addTab(thirdText);
+        TabHost.TabSpec fourthText = tabhost.newTabSpec("NOTIFICATIONS");
+        fourthText.setContent(R.id.NOTIFICATIONS);
+        fourthText.setIndicator("",getDrawable(R.drawable.ic_notifications_white_48dp));
+        tabhost.addTab(fourthText);
+        TabHost.TabSpec fifthText = tabhost.newTabSpec("NOTIFICATIONS");
+        fifthText.setContent(R.id.PROFILE);
+        fifthText.setIndicator("",getDrawable(R.drawable.ic_person_white_48dp));
+        tabhost.addTab(fifthText);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
